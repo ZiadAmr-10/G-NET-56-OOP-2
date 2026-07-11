7567f66978bedf60d7ac7d8ec0d6c85b8e8ee148
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-07-11 17:59:49 +0300

    Question01

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+            #region Q01
+            /*
+        a)1-No protection for data because the fields are public and can Access the felds directly and can modify it
+          2-No validation to validate the amount before subtracting it from the balance.
+        b)1-make the fields private 
+          2-using properties to provide control access of fields
+          3-Validate the withdrawal amount before subtracting it from the balance 
+        c)Because public fields allow anyone outside the class to access and modify the data directly ,
+            and if another project uses this class from a DLL file, and I change a field, the other project must update its code.
+             */
+            #endregion
+            #endregion
         }
     }
 }
